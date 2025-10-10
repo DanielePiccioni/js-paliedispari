a91e501965f46a87b42847f66edde673973dc121
DanielePiccioni <danielepiccioni1@gmail.com>
2025-10-10 13:47:52 +0200

    palindroma-tools in italian

--- palindroma/palindroma.js
+++ palindroma/palindroma.js
@@ -3,3 +3,11 @@ Palidroma
 Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma
 */
+
+/*
+TOOLS:
+-chiedere alla persona una parola
+-creare una funzioni
+-controllare tramite la funzione se quella parola è palindroma
+-stampare il risultato
+*/
